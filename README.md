d7f6f7d80895ab1ce07a4e7cb6d14e8b092da49b
Bradley Bossard <bradleybossard@gmail.com>
2017-08-18 20:51:57 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # slack-animated-emoji
 A collection of animated Slack emojis
+
+# Purposes
+
+  Creates browsable pages of all animated emojis in ./gifs directory
+
+# Install
+
+    mkvirtualenv <env-name>          # optional
+    pip install -r requirements.txt  # install jinja and any other dependencies
+    # Add new gifs to ./gifs directory
+    python build.py
---
+++ build.py
@@ -0,0 +1,36 @@
+import jinja2
+import os
+
+directory_name = './gifs/'
+width = 32
+height = 32
+
+files = sorted(os.listdir(directory_name))
+
+templateLoader = jinja2.FileSystemLoader( searchpath='./templates' )
+templateEnv = jinja2.Environment( loader=templateLoader )
+
+TEMPLATE_FILE = 'index.jinja'
+template = templateEnv.get_template( TEMPLATE_FILE )
+
+
+letters = []
+for char in '0123456789abcdefghijklmnopqrstuvwxyz':
+    emojis = []
+    for file in files:
+        if file[0] == char:
+            basename = os.path.splitext(file)[0]
+            emojis.append({'directory_name': directory_name,
+                           'file': file,
+                           'width': width,
+                           'height': height,
+                           'basename': basename})
+    if len(emojis) > 0:
+        letters.append({'char': char, 'emojis': emojis})
+
+templateVars = { 'letters': letters }
+
+output_text = template.render(templateVars)
+
+with open("index.html", "w") as text_file:
+        text_file.write(''.join(output_text))
